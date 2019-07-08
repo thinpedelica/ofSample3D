2a6f7cc94d69862bfbe5bc6e860df81908d77c6b
thinpedelica <thinpedelica@gmail.com>
2019-07-08 16:26:03 +0900

    feat: 3d model loading sample.

---
+++ ofSample3D/src/ofApp.cpp
@@ -0,0 +1,40 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetVerticalSync(true);
+    ofSetFrameRate(30);
+    ofSetBackgroundColor(0);
+
+    if (!model_loader_.loadModel("model/12337_Statue_v1_l1.obj")) {
+        std::cout << "##### load fail." << std::endl;
+        return;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    static float rotate = 0.f;
+    //model_loader_.setRotation(2, rotate, 0, 0, 1);
+
+    rotate += 0.2f;
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    cam_.begin();
+    cam_.setPosition(0.f, 500.f, 1000.f);
+    cam_.lookAt(ofVec3f(0.f, 300.f, 0.f), ofVec3f(0.f, 1.f, 0.f));
+
+    model_loader_.setPosition(0, 100, 0);
+    model_loader_.setScale(1.f, 1.f, 1.f);
+
+    model_loader_.disableTextures();
+    glPointSize(.1f);
+    model_loader_.drawVertices();
+
+    //glLineWidth(.1f);
+    //model_loader_.drawWireframe();
+
+    cam_.end();
+}
---
+++ ofSample3D/src/ofApp.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxAssimpModelLoader.h"
+
+class ofApp : public ofBaseApp{
+public:
+    void setup();
+    void update();
+    void draw();
+
+private:
+    ofxAssimpModelLoader model_loader_;
+    ofEasyCam cam_;
+};
